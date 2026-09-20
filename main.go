9147afc5272e062399525777f7a2d8295f8517ce
rruslek <107173060+rruslek@users.noreply.github.com>
2026-09-20 22:15:40 +0500

    feat: initial commit with basic telegram bot

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	tg_token := os.Getenv("TELEGRAM_BOT_TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(tg_token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates := bot.GetUpdatesChan(u)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		log.Println("Shutting down...")
+		bot.StopReceivingUpdates()
+	}()
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.IsCommand() {
+			if update.Message.Command() == "start" {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Начинаю принимать сообщения. Отправьте мне текст, и я повторю его.")
+				msg.ReplyToMessageID = update.Message.MessageID
+				if _, err := bot.Send(msg); err != nil {
+					log.Println(err)
+				}
+			}
+			if update.Message.Command() == "stop" {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Останавливаю прием сообщений... До свидания!")
+				msg.ReplyToMessageID = update.Message.MessageID
+				if _, err := bot.Send(msg); err != nil {
+					log.Println(err)
+				}
+				log.Println("Shutting down...")
+				bot.StopReceivingUpdates()
+			}
+		} else {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
